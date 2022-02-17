db306394ab27045b99f9952cf9075d4c89f5a439
thejas97 <thejasgowda97@gmail.com>
2022-02-17 13:26:36 +0530

    Initial commit

---
+++ src/main/java/com/ty/hungryhelpers/hungryhelpers/HungryHelpersApplication.java
@@ -0,0 +1,13 @@
+package com.ty.hungryhelpers.hungryhelpers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungryHelpersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HungryHelpersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/hungryhelpers/hungryhelpers/HungryHelpersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.hungryhelpers.hungryhelpers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryHelpersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
